6726f0cfe7d13f07b978cf7ad45fb5cb1410bd98
Alekszee <Aleksgreen46@gmail.com>
2025-01-07 15:10:03 +0300

    Введение в функциональное программирование

---
+++ main.py
@@ -0,0 +1,15 @@
+def apply_all_func(int_list, *functions) :
+    results = {}
+    for function in functions :
+        results[function.__name__] = function(int_list)
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+# Вывод на консоль:
+# {'max': 20, 'min': 6} {'len': 4, 'sum': 50, 'sorted': [6, 9, 15, 20]}
+
+
